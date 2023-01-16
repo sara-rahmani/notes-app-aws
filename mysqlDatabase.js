3fd18cf68ae64ccf443a633abfa0885885483d3c
sara rahmani <sararahmani96@gmail.com>
2023-01-16 01:23:38 -0800

    notes app

--- mysqlDatabase.js
+++ mysqlDatabase.js
@@ -3,14 +3,18 @@
 const mysql = require("mysql2")
 const dotenv = require("dotenv")
 dotenv.config()
-
+MYSQLHOST='containers-us-west-55.railway.app'
+MYSQLUSER='root'
+MYSQLPASSWORD='UC1n99rNlo2fehVK2DfZ'
+MYSQLDATABASE='railway'
+MYSQLPORT='6599'
 const pool = mysql
   .createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT ,
+    host: MYSQLHOST,
+    user:MYSQLUSER,
+    password: MYSQLPASSWORD,
+    database: MYSQLDATABASE,
+    port: MYSQLPORT ,
     // socketPath: '/var/run/mysqld/mysqld.sock',
 
 })
