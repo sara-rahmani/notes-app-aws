aecb8909bf361e74a8ca995e47d87423319801d5
sara rahmani <sararahmani96@gmail.com>
2023-01-16 01:17:57 -0800

    notes app

--- mysqlDatabase.js
+++ mysqlDatabase.js
@@ -10,7 +10,7 @@ const pool = mysql
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
+    port: process.env.MYSQLPORT ,
     // socketPath: '/var/run/mysqld/mysqld.sock',
 
 })
