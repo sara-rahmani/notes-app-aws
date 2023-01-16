732b6e85bca92f5405af2fd689b9d847306cd05e
sara rahmani <sararahmani96@gmail.com>
2023-01-16 00:46:33 -0800

    notes app

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ console.log(id);
 })
 // After all other routes
 app.get('*', (req, res) => {
-  res.sendFile('index.ejs');
+  res.sendFile('views/index.ejs');
 });
 
 const port = process.env.PORT || 8081
